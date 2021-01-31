59bbe1af7038006de43d143a1e1217db1bd664e7
Purva Sheth <purvashet@gmail.com>
2021-02-01 00:11:51 +0530

    feat:update 26*26 table to 95*95 by including numbers and special chars

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-// Make the 26*26 table for Vigenere Cipher
-var chars = [...Array(26).keys()];
+/* Using 95*95 table for Vigenere Cipher. 
+ASCII characters from 32 - 126 are considered.
+*/
+var n = 95;
+var chars = [...Array(n).keys()];
 var Table = [];
 Table.push([...chars]);
-for (var i = 1; i < 26; i++) {
+for (var i = 1; i < n; i++) {
   front = chars.shift();
   chars.push(front);
   Table.push([...chars]);
 }
 
 function getChar(i, j) {
-  console.log(Table[0]);
-  return String.fromCharCode(65 + Table[i][j]);
+  return String.fromCharCode(32 + Table[i][j]);
 }
 
 function getNum(c) {
-  return c.charCodeAt(0) - 65;
+  return c.charCodeAt(0) - 32;
 }
 
 function encryption(P, K) {
-  console.log(getNum(P), getNum(K));
   return getChar(getNum(P), getNum(K));
 }
 
 function decryption(C, K) {
   // find C in Kth column and get char at same index 0th column
   var j = getNum(K);
-  for (var i = 0; i < 26; i++) {
+  for (var i = 0; i < n; i++) {
     if (getNum(C) == Table[i][j]) {
-      console.log(i, j);
       return getChar(i, 0);
     }
   }
@@ -40,20 +40,20 @@ var encrypt = document.querySelector("#encrypt");
 var decrypt = document.querySelector("#decrypt");
 
 encrypt.addEventListener("click", () => {
-  var plainText = textarea.value.toUpperCase();
-  var secretKey = key.value.toUpperCase();
+  var plainText = textarea.value;
+  var secretKey = key.value;
   var j = 0;
   cipher = "";
   for (var i = 0; i < plainText.length; i++) {
     cipher = cipher + encryption(plainText[i], secretKey[j]);
     j = (j + 1) % secretKey.length;
   }
-  output.innerHTML = cipher.toLowerCase();
+  output.innerText = cipher;
 });
 
 decrypt.addEventListener("click", () => {
-  var cipher = textarea.value.toUpperCase();
-  var secretKey = key.value.toUpperCase();
+  var cipher = textarea.value;
+  var secretKey = key.value;
 
   var j = 0;
   plainText = "";
@@ -61,9 +61,5 @@ decrypt.addEventListener("click", () => {
     plainText = plainText + decryption(cipher[i], secretKey[j]);
     j = (j + 1) % secretKey.length;
   }
-  console.log(plainText);
-  output.innerHTML = plainText.toLowerCase();
+  output.innerHTML = plainText;
 });
-
-console.log(encryption("A", "K"));
-console.log(decryption("K", "K"));
